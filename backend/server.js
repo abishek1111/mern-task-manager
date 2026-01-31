1a7df87b3fa4924a445ae7f0fc2d953d5998444f
Abhishek Pandey <yourgithubemail@gmail.com>
2026-02-01 00:33:50 +0530

    Initial MERN Task Manager Project

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/taskdb")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const taskRoutes = require("./routes/taskRoutes");
+app.use("/tasks", taskRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
